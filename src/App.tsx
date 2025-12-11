7c2ed5bfafd9f898384d86d4648c8fbedf1ed02c
ClarissaCardarelli <ccardare@gmail.com>
2025-12-11 19:12:31 +0100

    Hero refactor and Css changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router";
-import Nav from "./components/nav";
+import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import "./css/Reset.css";
 import "./css/Variables.css";
 import "./css/Typography.css";
 import "./css/App.css";
+import "./css/mobile.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,39 +2,44 @@ import "../css/Hero.css";
 
 function Hero() {
   return (
-    <section className="hero-bg">
-      <div className="hero-container">
+    <header className="hero-bg">
+      <section className="hero-container">
         <p className="body-text">Bonjour, je suis</p>
         <h1 className="primary-title gradient-text">Clarissa Cardarelli</h1>
-        <p className="job-title">Développeur Full Stack </p>
+        <h2>Développeur Full Stack</h2>
         <p className="body-text">
           Je suis une passionnée de création d'expériences web esthétiques et
           conviviales. Je suis prête à apporter de nouvelles perspectives et des
           solutions créatives à votre équipe.
         </p>
-
         <div className="hero-buttons-container">
           <a
             href="https://github.com/ClarissaCardarelli"
             className="body-text btn blue-btn"
             target="_blank"
+            rel="noopener noreferrer"
           >
-            <i className="bi bi-github btn-icon" /> GitHub
+            <i className="bi bi-github btn-icon" aria-hidden="true" /> GitHub
           </a>
           <a
             href="https://www.linkedin.com/in/clarissa-cardarelli/"
             className="body-text btn green-btn"
             target="_blank"
+            rel="noopener noreferrer"
           >
-            <i className="bi bi-linkedin btn-icon" /> Linkedin
+            <i className="bi bi-linkedin btn-icon" aria-hidden="true" />
+            Linkedin
           </a>
           <button type="button" className="body-text btn orange-btn">
-            <i className="bi bi-file-earmark-arrow-down btn-icon" /> Télécharger
-            mon CV
+            <i
+              className="bi bi-file-earmark-arrow-down btn-icon"
+              aria-hidden="true"
+            />
+            Télécharger CV
           </button>
         </div>
-      </div>
-    </section>
+      </section>
+    </header>
   );
 }
 
